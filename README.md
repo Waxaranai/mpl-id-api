d3b6e3acb220bb902dce9112818396b3f5f19df2
Waxaranai <waxaranai@gmail.com>
2022-08-12 11:02:54 +0000

    feat: initial commit

--- README.md
+++ README.md
@@ -1,19 +1,6 @@
 <div align="center">
 
-<img src="https://api.frutbits.org/assets/images/logo.webp" alt="FrutBits Indonesia Logo" width="200px" height="200px"/>
+<img src="https://telegra.ph/file/09d8131679b7f5f6cae02.jpg" alt="MPL Indonesia banner" style="border-radius: 1rem"/>
 
-# @frutbits/template
-
-**Template for FrutBits Indonesia Repositories**
-
-[![GitHub](https://img.shields.io/github/license/frutbits/template)](https://github.com/frutbits/template/blob/main/LICENSE)
-[![GitHub version](https://badge.fury.io/gh/frutbits%2Ftemplate.svg)](https://badge.fury.io/gh/frutbits%2Ftemplate)
-[![Discord](https://discord.com/api/guilds/332877090003091456/embed.png)](https://frutbits.org/discord)
-
-</div>
-
-# Steps after creating a repo with this template
-
-1. Find and replace all instances of `template` with the proper word / ID / etc
-1. Ensure the primary branch is called `main`
-1. Remove / change this section from the README
+# Jadwal MPL API
+API for MPL Indonesia Season 10 Schedule
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,55 @@
-export const main = (): string => "Hello World!";
+import got from "got";
+import express from "express/index";
+import { load } from "cheerio";
 
-console.log(main());
+const PORT = process.env.PORT ?? 3000;
+const BASE_URL = "https://id-mpl.com";
+const server = express();
+
+interface IMatchResult {
+    team: string;
+    date: string;
+    score?: string;
+    icon: { teamOne: string; teamTwo: string };
+}
+
+const getJadwal = async (): Promise<IMatchResult[]> => {
+    const response = await got({ url: BASE_URL });
+    const $ = load(response.body);
+    const results: IMatchResult[] = $(".match-box").map((_index, element) => {
+        const teamOneElement = $(element).find("div.match-team1");
+        const teamTwoElement = $(element).find("div.match-team2");
+        const teamOneName = teamOneElement.find("div > .team-name").text();
+        const teamTwoName = teamTwoElement.find("div > .team-name").text();
+        const dateAndTime = $(element).find(".championship").map((_, champElement) => $(champElement).text()
+            .replace(/\n/g, "")
+            .trim())
+            .toArray()
+            .join(" ");
+
+        const score = $(element).find(".font-condensed").map((_, scoreElement) => $(scoreElement).text()
+            .replace(/\n/g, "")
+            .trim())
+            .toArray();
+
+        const match: IMatchResult = {
+            team: `${teamOneName} vs ${teamTwoName}`,
+            date: dateAndTime,
+            score: score.length > 0 ? score.join(" ") : undefined,
+            icon: {
+                teamOne: String(teamOneElement.find("div > img").attr("src")),
+                teamTwo: String(teamTwoElement.find("div > img").attr("src"))
+            }
+        };
+        return match;
+    }).toArray();
+    return results;
+};
+server.get("/", (_req, res) => res.redirect("/api/jadwal"));
+
+server.get("/api/jadwal", async (_req, res) => {
+    const results = await getJadwal();
+    res.status(200).json({ statusCode: 200, results });
+});
+
+server.listen(PORT, () => console.info(`Server listening to: ${PORT}`));
