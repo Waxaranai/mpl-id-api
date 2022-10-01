b1ca0c91ef5d56bf07f5dc80b8d1b8b6cc90cb6c
Rewtio <mohammadbrianabdillah@gmail.com>
2022-10-01 19:54:31 +0700

    feat: change date format
    preview changes:
    before: 1 Okt 15:00
    after: 01-10-2022 15:00

--- src/utils/getCurrentWeekSchedule.ts
+++ src/utils/getCurrentWeekSchedule.ts
@@ -1,9 +1,12 @@
 import got from "got";
 import { load } from "cheerio";
+import moment from "moment-timezone";
 import { BASE_URL } from "../config";
 import { IMatchResult } from "../typings";
 
 const request = got.extend({ prefixUrl: BASE_URL });
+const monthShort = ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des"];
+const DATE_FORMAT = "DD MM YYYY mm:ss";
 
 export const getCurrentWeekSchedule = async (): Promise<IMatchResult[]> => {
     const response = await request({ });
@@ -13,11 +16,17 @@ export const getCurrentWeekSchedule = async (): Promise<IMatchResult[]> => {
         const teamTwoElement = $(element).find("div.match-team2");
         const teamOneName = teamOneElement.find("div > .team-name").text();
         const teamTwoName = teamTwoElement.find("div > .team-name").text();
-        const dateAndTime = $(element).find(".championship").map((_, champElement) => $(champElement).text()
-            .replace(/\n/g, "")
-            .trim())
+        const dateAndTime = $(element).find(".championship").map((_, champElement) => {
+            let str = $(champElement).text().replace(/\n/g, "")
+                .trim();
+            const shortMonth = monthShort.findIndex(v => str.includes(v));
+            if (shortMonth > -1) {
+                str = str.replace(monthShort[shortMonth], (shortMonth + 1).toString());
+            }
+            return str;
+        })
             .toArray()
-            .join(" ");
+            .join(` ${new Date().getFullYear()}`);
 
         const score = $(element).find(".font-condensed").map((_, scoreElement) => $(scoreElement).text()
             .replace(/\n/g, "")
@@ -26,7 +35,7 @@ export const getCurrentWeekSchedule = async (): Promise<IMatchResult[]> => {
 
         const match: IMatchResult = {
             team: `${teamOneName} vs ${teamTwoName}`,
-            date: dateAndTime,
+            date: moment(dateAndTime, DATE_FORMAT).format("DD-MM-YYYY mm:ss"),
             score: score.length > 0 ? score.join(" ") : undefined,
             icon: {
                 teamOne: String(teamOneElement.find("div > img").attr("src")).replace(/\s/g, "%20"),
